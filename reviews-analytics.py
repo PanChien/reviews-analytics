9c48582875bef563827b24f7622b27ccf76f3da2
PanChien <zoo0605@hotmail.com>
2020-10-31 16:00:23 +0800

    add progressbar2

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -1,13 +1,17 @@
+import time
+import progressbar
+
 data = []
 count = 0
-
+bar = progressbar.ProgressBar(max_value=1000000)
 #開始檔案，並顯示讀取進度後，再顯示data內有幾筆資料
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1  # count = count + 1
-		if count % 1000 == 0:	#如果count除1000的餘數 等於 0
-			print(len(data))
+		bar.update(count)
+		# if count % 1000 == 0:	#如果count除1000的餘數 等於 0
+		# 	print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
 #計算留言的平均長度
@@ -43,6 +47,7 @@ print(good[0])
 
 
 # 文字計數
+start_time = time.time()
 wc = {}    # 字典 word_count
 for d in data:            #清單data
 	words = d.split(' ')  #split()預設為空白鍵，將d分割後(又變成清單)再存入words
@@ -57,7 +62,8 @@ for d in data:            #清單data
 for word in wc:
 	if wc[word] > 1000000:
 		print(word,wc[word])
-
+end_time = time.time()
+print('文字計數共花了',end_time - start_time,'秒。')
 
 #字典的長度
 print('字典的長度：',len(wc))
